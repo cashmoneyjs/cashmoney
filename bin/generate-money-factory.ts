526bd2ea9acf42f559e9ea23fde1fa28bd97fb5c
Matthew Gamble <git@matthewgamble.net>
2020-02-03 09:14:36 +1100

    Turn on esModuleInterop

--- bin/generate-money-factory.ts
+++ bin/generate-money-factory.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
+import fs from "fs";
+import path from "path";
 import Currency from "../src/currency";
 import ISOCurrencyList from "../src/currencylists/iso";
 import BitcoinCurrencyList from "../src/currencylists/bitcoin";
