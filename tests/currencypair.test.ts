842682de800bb34a532fe9b7330d7a899fffe435
Matthew Gamble <git@matthewgamble.net>
2020-02-03 20:22:14 +1100

    Add test for currencypair factory function

--- tests/currencypair.test.ts
+++ tests/currencypair.test.ts
@@ -5,6 +5,13 @@ import CurrencyPair from "src/currencypair";
 
 @TestFixture("Currency Pair")
 export default class CurrencyPairTest {
+    @Test("it parses ISO currency pair strings")
+    public itParsesIsoCurrencyPairStrings() {
+        const pair = new CurrencyPair(new Currency("EUR"), new Currency("USD"), 1.25);
+        const isoPair = CurrencyPair.createFromIso("EUR/USD 1.25");
+        Expect(isoPair).toBe(pair);
+    }
+
     @Test("it converts to JSON")
     public itConvertsToJson() {
         const expectedJson = '{"baseCurrency":"EUR","counterCurrency":"USD","ratio":1.25}';
